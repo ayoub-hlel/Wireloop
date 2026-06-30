b67049c5990f79fd9f9ed11b8fa13a5e17cd0d32
Biyop <ayoub.hlel@proton.me>
2026-06-30 10:43:29 +0100

    env/ restored

---
+++ env/env.prod.ts
@@ -0,0 +1,6 @@
+export default {
+  server_arduino_url: "https://compile.arduino-workflow-builder.org",
+  bucket_name: "arduino-workflow-builder-lesson-test",
+  useEmulator: false,
+  site: "arduino-workflow-builder-org",
+};
